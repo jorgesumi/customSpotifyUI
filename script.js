8c4f9d7fa503099994e335296ac091b7440cec3a
Jorge Sumi <jorgesumi@gmail.com>
2023-02-10 16:53:50 -0500

    Began Node Setup

--- script.js
+++
@@ -1,8 +0,0 @@
-var score = document.getElementById("score");
-var output = document.getElementById("demo")
-output.innerHTML = score.value; // Display the default slider value
-
-// Update the current slider value (each time you drag the slider handle)
-score.oninput = function() {
-    output.innerHTML = this.value;
-}
