9796ccbc56a13a7d02252227f3318b7ff7625e06
Jorge Sumi <jorgesumi@gmail.com>
2023-02-14 21:51:01 -0500

    Updated node packages

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
-var http = require('http');
-var fs = require('fs');
+const express = require('express');
+const app = express();
+const port = 8080
+const http = require('http');
+const fs = require('fs');
 
+app.get('', (req, res) => {
+    res.sendFile(__dirname + '/views/index.html')
+})
 http.createServer(function (req, res) {
     fs.readFile('app.html', function (err, data) {
         res.writeHead(200, {'Content-Type': 'text/html'});
